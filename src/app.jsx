bed22344f727c0a6521dfcf9d1325da7b5662596
shj3497 <shj3497@gmail.com>
2021-06-17 15:54:03 +0900

    Youtube에서 동영상제목 받아오기

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import VideoList from './components/video_list/video_list';
+import './app.css';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyBXmi_7XYzOSL8o3-0gMWmOkWbfnLHdU4o", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, [])
+  
+  return (
+    <VideoList videos={videos} />
+  );
+}
+
+export default App;
